9c9b5b7d2e201379c8046ef59a0fec0369e237a2
Caio Sa <caiohenriquesa1@gmail.com>
2021-11-18 23:08:07 -0300

    second commit

--- go/index.js
+++ go/index.js
@@ -1,7 +1,5 @@
 function validation(){
   let email_input = document.getElementById('exampleFormControlInput1').value;
-//  let textarea_input = document.getElementById('exampleFormControlTextarea1').value;
-  console.log(email_input);
   let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if(re.test(email_input)){
   } else {
